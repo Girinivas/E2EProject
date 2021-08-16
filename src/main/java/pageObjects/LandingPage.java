dec15e92f60d2a600de20be18927683e62aa2a74
Girinivas <girinivasnm@gmail.com>
2021-08-16 14:36:14 +0530

    second commit

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 //rahulonlinetutor@gmail.com
+//comment
 public class LandingPage {
 
 	
